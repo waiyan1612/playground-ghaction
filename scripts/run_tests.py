a6476f98e525d86abb3c3f7bb6ff61cb13ea48e6
Wai Yan <waiyan1612@gmail.com>
2025-10-21 14:23:47 +0800

    Update run_tests.py

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,12 +1,17 @@
+import shutil
 import os
+import re
 
 print("Running test cases")
 artifact_dir = os.path.join(os.path.dirname(__file__), "../artifacts")
 
 code_files = os.listdir(artifact_dir)
 print(f"Choosing best variant from {code_files}")
-best = os.path.join("./artifacts", code_files[1])
-print(f"Selected: {best}")
+selected = os.path.join("./artifacts", code_files[1])
+
+best = re.sub(r'-variant\d+(?=\.py$)', '', selected)
+shutil.copy(selected, best)
+print(f"Promoted {selected} to {best}")
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
     f.write(f"best={best}\n")
