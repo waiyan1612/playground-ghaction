7e0bfd5d6a523f19c67a54ea6ca4488b2d707d3f
Wai Yan <waiyan1612@gmail.com>
2025-10-21 18:09:29 +0800

    remove

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,4 +1,3 @@
-import shutil
 import os
 import re
 
@@ -6,15 +5,12 @@ print("Running test cases")
 artifact_dir = os.path.join(os.path.dirname(__file__), "../artifacts")
 
 code_files = os.listdir(artifact_dir)
-print(f"Choosing best variant from {code_files}")
-selected = os.path.join("./artifacts", code_files[1])
-
-print("Summarizing results")
-with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
+with open("test_results.md", "w") as f:
     f.write("| Filename | Passed | Failed |\n| -------- | ------ | ------ |\n")
     for code_file in code_files:
       f.write(f"|{code_file}| xx | yy |\n")
 
-best = re.sub(r'-variant\d+(?=\.py$)', '', selected)
-shutil.copy(selected, best)
+selected = os.path.join("./artifacts", code_files[1])
+with open("selected.txt", "w") as f:
+    f.write(selected)
 print(f"Promoted {selected} to {best}")
