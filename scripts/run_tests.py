ce91ad67d2808554a0bb429c04029ecf3cfa972d
Wai Yan <waiyan1612@gmail.com>
2025-10-21 14:10:31 +0800

    Update run_tests.py

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -3,14 +3,9 @@ import os
 print("Running test cases")
 artifact_dir = os.path.join(os.path.dirname(__file__), "../artifacts")
 
-code_files = [
-    f
-    for f in os.listdir(artifact_dir)
-    if os.path.isfile(os.path.join(artifact_dir, f))
-]
-
+code_files = os.listdir(artifact_dir)
 print(f"Choosing best variant from {code_files}")
-best = code_files[1]
+best = os.path.join("./artifacts", code_files[1])
 print(f"Selected: {best}")
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as f:
