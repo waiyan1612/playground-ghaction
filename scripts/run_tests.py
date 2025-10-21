c6f9c8b5b123c41d0c5a353f909e791a9a4e2e11
Wai Yan <waiyan1612@gmail.com>
2025-10-21 16:31:18 +0800

    Update run_tests.py

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -9,6 +9,12 @@ code_files = os.listdir(artifact_dir)
 print(f"Choosing best variant from {code_files}")
 selected = os.path.join("./artifacts", code_files[1])
 
+print("Summarizing results")
+with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
+    f.write("| Filename | Passed | Failed |\n| -------- | ------ | ------ |\n")
+    for code_file in code_files:
+      f.write(f"|{code_file}| xx | yy |\n")
+
 best = re.sub(r'-variant\d+(?=\.py$)', '', selected)
 shutil.copy(selected, best)
 print(f"Promoted {selected} to {best}")
