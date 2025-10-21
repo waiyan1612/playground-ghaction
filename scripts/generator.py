35dae722e810ca6d70f85117c7e6b3b388590d1d
Wai Yan <waiyan1612@gmail.com>
2025-10-21 15:40:18 +0800

    Update generator.py

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,6 +1,7 @@
-print(
-"""
+import random
+cities = ["Tokyo", "Paris", "Jakarta", "Seoul", "Singapore", "London", "Sydney", "Toronto"]
+
+print("""
 def hello():
-  print('Hello world')
-"""  
-)
+  print(f"Hello {random.choice(cities)}")
+""")
